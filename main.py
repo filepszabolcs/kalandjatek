d1ec342b04d753e49bee4414ebea967e6634a015
czimi <czimi@hotmail.com>
2021-06-10 12:00:04 +0200

    ötös javítva

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ while i != 19 or i != 22:
         if v == 5 or v == 6 or v == 7:
             i = v
 
+
     if i == 3:
         print(
             "8. az akciód nem jól sül el, mivel az égő csikktől meggyullad a szemét és lángra kap az egész bejárati ajtó.\n"
@@ -45,6 +46,10 @@ while i != 19 or i != 22:
             "12. kiszúrod az éppen közeledő dögös csajt az évfolyamról és megszólítod.\n"
             "14. kihúzod magad a tanár kezei közül és röhögve elfutsz előle a folyosón.\n"
             "Mit választasz? Írj be a számot:")
+
+    v = int(input())
+    if v == 10 or v == 12 or v == 14:
+        i = v
 
     if i == 7:
         print("12. kiszúrod az éppen közeledő dögös csajt az évfolyamról és megszólítod. \n"
