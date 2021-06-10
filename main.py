cbcd91b69e7ff9303c06ec0bb713cf2a004cbc9b
panniangyan <panniangyan@gmail.com>
2021-06-10 11:36:12 +0200

    point 6 clone

--- main.py
+++ main.py
@@ -19,3 +19,12 @@ while i != 19 or i != 22:
         v = int(input())
         if v == 5 or v == 6 or v == 7:
             i = v
+
+    if i==6:
+        print ( "4. odaér a haverod, Béci is. Rágyújtotok egy újabb cigire.\n"
+                "15. megbököd a vállát és megkínálod cigivel, elfogadja és rágyújt.\n"
+                "16. Bemész a suliba. Feszkós vagy, ezért betörsz egy ablakot a folyosón.\n"
+                "Mit választasz? Írj be a számot: ")
+        v = int(input())
+        if v == 4 or v == 15 or v == 16:
+            i = v
