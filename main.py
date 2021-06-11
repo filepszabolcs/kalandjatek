08b2197cbab1bfb7b30635a7fa09c12a2c09d6df
Szabi <filep.szabolcs@apertus.uni-nke.hu>
2021-06-11 09:27:56 +0200

    remove duplicated 17 point

--- main.py
+++ main.py
@@ -159,13 +159,6 @@ while i != 19 or i != 22:
             print("VÉGE A SZTORINAK")
             i = v
 
-        if i == 20:
-            print("22. a nap hátralévő részében disznó vicceken röhögtök...\n"
-                  "Mit választasz? Írj be a számot:")
-            v = int(input())
-            if v == 22:
-                i = v
-
     if i == 20:
         print("22. a nap hátralévő részében disznó vicceken röhögtök...\n"
               "Mit választasz? Írj be a számot:")
